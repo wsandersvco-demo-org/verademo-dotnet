de9d9f8300f9fa398c3bd92cd28628a3f1d432c9
demo <demo@TeamCity>
2026-05-06 13:55:09 +0000

    'Veracode Pipeline Scan + SCA' build configuration settings were updated (TeamCity change in 'verademo-dotnet' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -54,7 +54,6 @@ object VeracodePipelineScanSca : BuildType({
         powerShell {
             name = "Download Veracode CLI"
             id = "Veracode_CLI"
-            enabled = false
             scriptMode = script {
                 content = """
                     # Get the first file in the directory                   
